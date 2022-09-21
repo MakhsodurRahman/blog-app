5d014e7ca57af7d4f3465bea13510266d862de14
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 00:45:47 +0600

    bean validation code change

--- src/main/java/com/blogapp/entity/User.java
+++ src/main/java/com/blogapp/entity/User.java
@@ -12,7 +12,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
+    @Column(name = "user_id")
     private Long id;
 
     @Column(name = "name")
