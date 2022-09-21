43294f71b62be58fde6a5cb65f040ce2adf9937b
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-21 17:07:07 +0600

    add UserRepository

---
+++ src/main/java/com/blogapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.blogapp.repository;
+
+import com.blogapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
