4c432ed14018c21195d8b7019d9f082dc3dd5b89
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-21 17:09:10 +0600

    add create,update, getAll, delete, and create model mapper method

---
+++ src/main/java/com/blogapp/service/definition/UserService.java
@@ -0,0 +1,15 @@
+package com.blogapp.service.definition;
+
+import com.blogapp.dto.UserRequestDto;
+import com.blogapp.dto.UserResponseDto;
+
+import java.util.List;
+
+public interface UserService {
+    UserRequestDto createUser(UserRequestDto userRequestDto);
+    UserRequestDto updateUser(Long id, UserRequestDto userRequestDto);
+    UserRequestDto getUser(Long id);
+    List<UserRequestDto> getAllUsers();
+    void deleteUser(Long id);
+
+}
