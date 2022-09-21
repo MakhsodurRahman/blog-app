71cd5e792b49114fd912f5514f24d239579eefee
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-21 21:32:22 +0600

    data type change

--- src/main/java/com/blogapp/exception/ResourceNotFoundException.java
+++ src/main/java/com/blogapp/exception/ResourceNotFoundException.java
@@ -1,15 +1,17 @@
 package com.blogapp.exception;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class ResourceNotFoundException extends RuntimeException{
 
     public String resourceName;
     public String fieldName;
     public Long fieldValue;
     public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue){
-        super(String.format("%s not found with %s : %l",resourceName,fieldName,fieldValue));
+        super(String.format("%s not found with %s : %s", resourceName,fieldName,fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
