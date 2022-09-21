7e57e2328c3bb5fea8ccdcb0db944b03fa3edb13
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 00:00:50 +0600

    add bean validation annotation

--- src/main/java/com/blogapp/controller/definition/UserController.java
+++ src/main/java/com/blogapp/controller/definition/UserController.java
@@ -4,6 +4,7 @@ import com.blogapp.dto.UserRequestDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/api/v1")
@@ -11,7 +12,7 @@ public interface UserController {
 
 
     @PostMapping("/createUser")
-    public ResponseEntity<UserRequestDto> createUser(@RequestBody UserRequestDto userRequestDto);
+    public ResponseEntity<UserRequestDto> createUser(@Valid  @RequestBody UserRequestDto userRequestDto);
 
     @GetMapping("/getAllUser")
     public List<UserRequestDto> getAllUser();
@@ -20,7 +21,7 @@ public interface UserController {
     public ResponseEntity<UserRequestDto> getUser(@PathVariable Long id);
 
     @PutMapping("/updateUser/{id}")
-    public ResponseEntity<UserRequestDto> updateUser(@RequestBody UserRequestDto userRequestDto,@PathVariable Long id);
+    public ResponseEntity<UserRequestDto> updateUser(@Valid @RequestBody UserRequestDto userRequestDto,@PathVariable Long id);
 
     @DeleteMapping("/deleteUser/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id);
