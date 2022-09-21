b48813e74901620b0e2158187645f552de83c1f4
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-21 11:11:01 +0600

    create entity class

---
+++ src/main/java/com/blogapp/entity/User.java
@@ -0,0 +1,29 @@
+package com.blogapp.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "about")
+    private String about;
+}
