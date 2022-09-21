a481fb26c5cc11e15d3e560ed3d76b0ace42d32a
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-21 17:09:28 +0600

    add create,update, getAll, delete, and create model mapper method

---
+++ src/main/java/com/blogapp/service/implementation/UserServiceImpl.java
@@ -0,0 +1,83 @@
+package com.blogapp.service.implementation;
+
+import com.blogapp.dto.UserRequestDto;
+import com.blogapp.dto.UserResponseDto;
+import com.blogapp.entity.User;
+import com.blogapp.exception.ResourceNotFoundException;
+import com.blogapp.repository.UserRepository;
+import com.blogapp.service.definition.UserService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserRequestDto createUser(UserRequestDto userRequestDto) {
+
+        User user = dtoToEntity(userRequestDto);
+        User saveUser = userRepository.save(user);
+        return entityToDto(saveUser);
+
+    }
+
+    @Override
+    public UserRequestDto updateUser(Long id, UserRequestDto userRequestDto) {
+        User user = userRepository.findById(id).orElseThrow((()->  new ResourceNotFoundException("user "," id ",id)));
+
+        user.setName(userRequestDto.getName());
+        user.setEmail(userRequestDto.getEmail());
+        user.setPassword(userRequestDto.getPassword());
+        user.setAbout(userRequestDto.getAbout());
+        User updateUser = userRepository.save(user);
+        UserRequestDto dto = entityToDto(updateUser);
+        return dto;
+    }
+
+    @Override
+    public UserRequestDto getUser(Long id) {
+        User user = userRepository.findById(id).orElseThrow((()->  new ResourceNotFoundException("user "," id ",id)));
+
+        UserRequestDto findUser = entityToDto(user);
+        return findUser;
+    }
+
+    @Override
+    public List<UserRequestDto> getAllUsers() {
+
+        List<User> users = userRepository.findAll();
+        List<UserRequestDto> dto = users.stream().map(user -> entityToDto(user)).collect(Collectors.toList());
+        return dto;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        User user = userRepository.findById(id).orElseThrow((()->  new ResourceNotFoundException("user "," id ",id)));
+        userRepository.delete(user);
+    }
+
+    //this is called model mapper
+    private User dtoToEntity(UserRequestDto userRequestDto){
+        User user = new User();
+        user.setId(userRequestDto.getId());
+        user.setName(userRequestDto.getName());
+        user.setEmail(userRequestDto.getEmail());
+        user.setAbout(userRequestDto.getAbout());
+        return user;
+    }
+
+    private UserRequestDto entityToDto(User user){
+        UserRequestDto userRequestDto = new UserRequestDto();
+        userRequestDto.setId(user.getId());
+        userRequestDto.setName(user.getName());
+        userRequestDto.setPassword(user.getPassword());
+        userRequestDto.setAbout(user.getAbout());
+        return userRequestDto;
+    }
+}
