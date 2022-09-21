6476de31a8d414faafd7be2054c18a90d23683f5
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 00:44:30 +0600

    create CategoryService and create CRUD method

---
+++ src/main/java/com/blogapp/service/definition/CategoryService.java
@@ -0,0 +1,15 @@
+package com.blogapp.service.definition;
+
+import com.blogapp.dto.categorydto.CategoryRequestDto;
+import com.blogapp.dto.categorydto.CategoryResponseDto;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    CategoryResponseDto createCategory(CategoryRequestDto categoryRequestDto);
+    CategoryResponseDto updateCategory(Long id,CategoryRequestDto categoryRequestDto);
+    void deleteCategory(Long id);
+    CategoryResponseDto getCategory(Long id);
+    List<CategoryResponseDto> getAllCategory();
+}
