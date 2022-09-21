518ce161654d8452ac7d834cba7b176cb3c3446e
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 00:01:06 +0600

    add bean validation annotation

--- src/main/java/com/blogapp/dto/UserRequestDto.java
+++ src/main/java/com/blogapp/dto/UserRequestDto.java
@@ -4,14 +4,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserRequestDto {
 
     private Long id;
+
+    @NotEmpty
+    @Size(min = 3, max = 10,message = "min 3 character & max 10 character")
     private String name;
+
+    @Email(message = "Email in not valid")
     private String email;
+
+    @NotEmpty
+    @Size(min = 3,max = 5,message = "min 3 and max 5 character")
     private String password;
+
+    @NotEmpty
     private String about;
 }
