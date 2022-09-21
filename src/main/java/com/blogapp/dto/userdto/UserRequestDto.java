2289d0368a5a7a61567df90d0cf05bcc8440f0c1
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 00:46:50 +0600

    some code change

--- src/main/java/com/blogapp/dto/userdto/UserRequestDto.java
+++ src/main/java/com/blogapp/dto/userdto/UserRequestDto.java
@@ -1,11 +1,10 @@
-package com.blogapp.dto;
+package com.blogapp.dto.userdto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
