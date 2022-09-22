42141b15d66ef31fad5a7aeea141c3e360058b1d
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 23:57:09 +0600

    getPostByUser,getPostByCategory, getAll post method created

--- src/main/java/com/blogapp/controller/definition/PostController.java
+++ src/main/java/com/blogapp/controller/definition/PostController.java
@@ -3,13 +3,22 @@ package com.blogapp.controller.definition;
 import com.blogapp.dto.post.PostRequestDto;
 import com.blogapp.dto.post.PostResponseDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/v1/post")
 public interface PostController {
 
-    @RequestMapping("/user/{userId}/category/{categoryId}/create")
+    @PostMapping("/user/{userId}/category/{categoryId}/create")
     public ResponseEntity<PostResponseDto> create(@RequestBody PostRequestDto postRequestDto, @PathVariable Long userId, @PathVariable Long categoryId);
+
+    @GetMapping("/user/{userId}/posts")
+    public ResponseEntity<List<PostResponseDto>> getPostsByUser(@PathVariable Long userId);
+
+    @GetMapping("/category/{categoryId}/posts")
+    public ResponseEntity<List<PostResponseDto>> getPostByCategory(@PathVariable Long categoryId);
+
+    @GetMapping("/getAllPost")
+    public ResponseEntity<List<PostResponseDto>> getAllPost();
 }
