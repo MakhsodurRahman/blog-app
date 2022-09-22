1ca95ca5f2c04a9a88461e611a3c3ad052ca7572
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 12:33:06 +0600

    Create CategoryRequestDto and apply bean validation

--- src/main/java/com/blogapp/dto/categorydto/CategoryRequestDto.java
+++ src/main/java/com/blogapp/dto/categorydto/CategoryRequestDto.java
@@ -4,11 +4,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
 @Getter
 @Setter
+@NoArgsConstructor
 public class CategoryRequestDto {
-
+    @NotEmpty
+    @Size(min = 3,message = "min size must be 3 character")
     private String categoryTitle;
+    @NotEmpty
+    @Size(min = 5,message = "min size must be 5 character")
     private String categoryDescription;
 }
