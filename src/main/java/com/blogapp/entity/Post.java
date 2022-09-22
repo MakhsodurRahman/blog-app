d26d6d29a47511a88130193cafb18b8f6a24d875
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 19:39:10 +0600

    create post entity

---
+++ src/main/java/com/blogapp/entity/Post.java
@@ -0,0 +1,37 @@
+package com.blogapp.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(name = "post_title",length = 100,nullable = false)
+    private String name;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "image_name")
+    private String imageName;
+
+    @Column(name = "add_date")
+    private Date addDate;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
