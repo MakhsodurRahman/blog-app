64a4bbc814306a3fb53df4408e4ba448b65d28f4
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 22:15:28 +0600

    title field name change and add user,category field

--- src/main/java/com/blogapp/dto/post/PostResponseDto.java
+++ src/main/java/com/blogapp/dto/post/PostResponseDto.java
@@ -1,8 +1,14 @@
 package com.blogapp.dto.post;
 
+import com.blogapp.dto.categorydto.CategoryResponseDto;
+import com.blogapp.dto.userdto.UserResponseDto;
+import com.blogapp.entity.Category;
+import com.blogapp.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -10,6 +16,10 @@ import lombok.Setter;
 public class PostResponseDto {
 
     private Long id;
-    private String title;
+    private String postTitle;
     private String content;
+    private String imageName;
+    private Date addDate;
+    private UserResponseDto user;
+    private CategoryResponseDto category;
 }
