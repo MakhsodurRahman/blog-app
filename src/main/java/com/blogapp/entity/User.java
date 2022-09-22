5221aabfe1cac16e2e07295d5c1bbff7408d1e20
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 19:38:45 +0600

    entity relationship post to user

--- src/main/java/com/blogapp/entity/User.java
+++ src/main/java/com/blogapp/entity/User.java
@@ -4,6 +4,8 @@ package com.blogapp.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -26,4 +28,7 @@ public class User {
 
     @Column(name = "about")
     private String about;
+
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Post> posts = new ArrayList<>();
 }
