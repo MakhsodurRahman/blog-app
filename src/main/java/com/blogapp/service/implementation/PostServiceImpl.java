7cc20bde691e071a3d7d921e29e70ebb2767bb15
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-22 22:16:24 +0600

    create post create method implementation

--- src/main/java/com/blogapp/service/implementation/PostServiceImpl.java
+++ src/main/java/com/blogapp/service/implementation/PostServiceImpl.java
@@ -1,17 +1,50 @@
 package com.blogapp.service.implementation;
 
 import com.blogapp.dto.post.PostRequestDto;
+import com.blogapp.dto.post.PostResponseDto;
+import com.blogapp.entity.Category;
 import com.blogapp.entity.Post;
+import com.blogapp.entity.User;
+import com.blogapp.exception.ResourceNotFoundException;
+import com.blogapp.repository.CategoryRepository;
+import com.blogapp.repository.PostRepository;
+import com.blogapp.repository.UserRepository;
 import com.blogapp.service.definition.PostService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostServiceImpl implements PostService {
+
+    private final ModelMapper modelMapper;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+
+    public PostServiceImpl(ModelMapper modelMapper, PostRepository postRepository, UserRepository userRepository, CategoryRepository categoryRepository) {
+        this.modelMapper = modelMapper;
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
     @Override
-    public Post createPost(PostRequestDto postRequestDto) {
-        return null;
+    public PostResponseDto createPost(PostRequestDto postRequestDto, Long userId, Long categoryId) {
+
+        User user = userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("user "," id ",userId));
+
+        Category category = categoryRepository.findById(categoryId).orElseThrow(()-> new ResourceNotFoundException("user "," id ",categoryId));
+
+        Post post = modelMapper.map(postRequestDto,Post.class);
+        post.setImageName("default.png");
+        post.setAddDate(new Date());
+        post.setUser(user);
+        post.setCategory(category);
+        Post savePost = postRepository.save(post);
+        return modelMapper.map(savePost,PostResponseDto.class);
     }
 
     @Override
