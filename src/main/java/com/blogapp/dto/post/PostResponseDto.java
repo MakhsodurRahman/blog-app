fc2d4ae4523a94f19de95749c67f16d58e75e409
Makhsodur-Rahman <makhsodurrahmanhridoy@gmail.com>
2022-09-25 21:02:43 +0600

    create post response

--- src/main/java/com/blogapp/dto/post/PostResponseDto.java
+++ src/main/java/com/blogapp/dto/post/PostResponseDto.java
@@ -1,14 +1,18 @@
 package com.blogapp.dto.post;
 
 import com.blogapp.dto.categorydto.CategoryResponseDto;
+import com.blogapp.dto.comments.CommentsResponseDto;
 import com.blogapp.dto.userdto.UserResponseDto;
 import com.blogapp.entity.Category;
+import com.blogapp.entity.Comments;
 import com.blogapp.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -20,6 +24,7 @@ public class PostResponseDto {
     private String content;
     private String imageName;
     private Date addDate;
+    private Set<CommentsResponseDto> comments = new HashSet<>();
     private UserResponseDto user;
     private CategoryResponseDto category;
 }
