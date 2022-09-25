eac7757074c9ee338136da3ae9cf1a468ce6a332
Makhsodur-Rahman <makhsodurrahmanhridoy@gmail.com>
2022-09-25 20:57:48 +0600

    implement interface generate encoded password

--- src/main/java/com/blogapp/BlogAppApplication.java
+++ src/main/java/com/blogapp/BlogAppApplication.java
@@ -1,12 +1,20 @@
 package com.blogapp;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class BlogAppApplication {
+public class BlogAppApplication implements CommandLineRunner {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public BlogAppApplication(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BlogAppApplication.class, args);
@@ -17,4 +25,8 @@ public class BlogAppApplication {
         return new ModelMapper();
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(passwordEncoder.encode("hri"));
+    }
 }
