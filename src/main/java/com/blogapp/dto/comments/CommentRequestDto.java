6f7d3c3db1e9dee3dde29e0ca86a03442b96b6c7
Makhsodur-Rahman <makhsodurrahmanhridoy@gmail.com>
2022-09-25 20:58:14 +0600

    create CommentRequestDto

---
+++ src/main/java/com/blogapp/dto/comments/CommentRequestDto.java
@@ -0,0 +1,13 @@
+package com.blogapp.dto.comments;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CommentRequestDto {
+
+    private String content;
+}
