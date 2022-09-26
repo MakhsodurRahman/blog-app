1a9df83c65c76f2fdec18b81cc789cf56e084a08
Makhsodur-Rahman <makhsodurrahmanhridoy@gmail.com>
2022-09-26 23:29:54 +0600

    set role in database

--- src/main/java/com/blogapp/BlogAppApplication.java
+++ src/main/java/com/blogapp/BlogAppApplication.java
@@ -1,5 +1,8 @@
 package com.blogapp;
 
+import com.blogapp.config.AppConstants;
+import com.blogapp.entity.Role;
+import com.blogapp.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,12 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
+
 @SpringBootApplication
 public class BlogAppApplication implements CommandLineRunner {
 
+    private final RoleRepository roleRepository;
+
     private final PasswordEncoder passwordEncoder;
 
-    public BlogAppApplication(PasswordEncoder passwordEncoder) {
+    public BlogAppApplication(RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -28,5 +36,23 @@ public class BlogAppApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println(passwordEncoder.encode("hri"));
+
+        try {
+                Role roleUser = new Role();
+                roleUser.setId(AppConstants.NORMAL_USER);
+                roleUser.setRoleName("NORMAL_USER");
+
+                Role roleAdmin = new Role();
+                roleAdmin.setId(AppConstants.ADMIN_USER);
+                roleAdmin.setRoleName("ADMIN_USER");
+                List<Role> roleList = List.of(roleUser, roleAdmin);
+                List<Role> result = roleRepository.saveAll(roleList);
+
+                for (Role role : result) {
+                    System.out.println(role.getRoleName());
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+        }
     }
 }
