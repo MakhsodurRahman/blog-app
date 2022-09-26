7cace60284b3160aa1bd36c880db6159fb17767d
Makhsodur-Rahman <makhsodurrahmanhridoy@gmail.com>
2022-09-26 20:18:08 +0600

    add EnableGlobalMethodSecurity for some method only access admin

--- src/main/java/com/blogapp/controller/definition/UserController.java
+++ src/main/java/com/blogapp/controller/definition/UserController.java
@@ -3,6 +3,7 @@ package com.blogapp.controller.definition;
 import com.blogapp.dto.userdto.UserRequestDto;
 import com.blogapp.dto.userdto.UserResponseDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,6 +25,7 @@ public interface UserController {
     @PutMapping("/updateUser/{id}")
     public ResponseEntity<UserResponseDto> updateUser(@Valid @RequestBody UserRequestDto userRequestDto,@PathVariable Long id);
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/deleteUser/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id);
 
